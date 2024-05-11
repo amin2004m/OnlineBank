792c94738546caa56585eae33b5b0c02e33a557a
amin2004m <aminsfsfd@gmail.com>
2024-05-12 00:11:41 +0400

    Entites created

--- src/main/java/az/bank/OnlineBank/controllers/UserController.java
+++ src/main/java/az/bank/OnlineBank/controllers/UserController.java
@@ -1,8 +1,5 @@
 package az.bank.OnlineBank.controllers;
 
-import az.bank.OnlineBank.entities.User;
-import az.bank.OnlineBank.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/az/bank/OnlineBank/repositories/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package az.bank.OnlineBank.repositories;
-
-public class UserRepository {
-}
